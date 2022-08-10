62e8b7ee45658aad1137fd43f6ed833014a8852e
Johnathan <johnathan.mnn@gmail.com>
2022-08-10 11:10:10 -0500

    Added all licenses and working on readme format

--- README.md
+++ README.md
@@ -1,1 +1,68 @@
-# readme-gen
+# + response.Name + 
+[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)
+
+1. [ Description. ](##description)
+2. [ Usage tips. ](#usage)
+d
+d
+d
+
+d
+d
+d
+d
+d
+d
+d
+d
+d
+d
+
+d
+d
+d
+d
+
+d
+d
+d
+d
+d
+
+d
+d
+d
+d
+d
+
+d
+d
+ddd
+d
+d
+d
+d
+
+
+
+d
+d
+d
+d
+d
+dd
+
+d
+d
+d
+dd
+
+d
+d
+d
+
+
+## Description
+Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+
+
--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 
 inquirer
-// array of objects
+// Questions to learn about the project adn fill out readme
   .prompt([
     {
       type: 'input',
@@ -16,7 +16,12 @@ inquirer
       },
       {
         type: 'input',
-        message: 'Can I have a link to your Github profile?',
+        message: 'What year is it?',
+        name: 'year',
+      },
+      {
+        type: 'input',
+        message: 'Whats your Github username?',
         name: 'github',
       },
       {
@@ -54,6 +59,12 @@ inquirer
         message: 'Whats do users need to know about making contributions?',
         name: 'contributions',
       },
+      {
+        type: 'input',
+        message: 'How should tests be run?',
+        name: 'tests',
+      },
+    //   This is a multiple choice question that allows the user to select a license
         {
         type: "list",
         message: "Choose your crust:",
@@ -62,10 +73,24 @@ inquirer
       },
   ])
   .then((response) =>{
-    if(response.license = "Apache 2.0"){
-        console.log("your mom");
+    // Depending on the license selected a different badge and notice will be displayed
+    if(response.license === "Apache 2.0"){
+        var licenseIcon = "[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)";
+        var fullLicense = "Copyright "+response.year+" "+ response.username +"\n\nLicensed under the Apache License, Version 2.0 (the 'License');\n\nyou may not use this file except in compliance with the License.\nYou may obtain a copy of the License at\n\nhttp://www.apache.org/licenses/LICENSE-2.0";
     }
-    fs.appendFile('index.html',"", (err) =>
+    if(response.license === "GNU GPL v3"){
+        var licenseIcon = "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)";
+        var fullLicense = "This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version. This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details. You should have received a copy of the GNU General Public License along with this program. If not, see <https://www.gnu.org/licenses/>.";
+    }
+    if(response.license === "MIT"){
+        var licenseIcon = "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)";
+        var fullLicense = "Copyright "+response.year+" "+ response.username +"\n\nPermission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the 'Software'), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:\n\nThe above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.\n\nTHE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.";
+    }
+    if(response.license === "BSD 3"){
+        var licenseIcon = "[![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)";
+        var fullLicense = "Copyright "+response.year+" "+ response.username +"\n\nRedistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:\n\n1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.\n\n2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.\n\n3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.\n\nTHIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS 'AS IS' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.";
+    }
+    fs.appendFile('fakeREADME.md', fullLicense, (err) =>
     err ? console.log(err) : console.log('Success!'))
   }
   );
